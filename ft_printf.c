44fd116f8beedea6452eebe74ad6c3f9b5ae61ed
Julio GONZALEZ GOMEZ <juligonz@e3r10p19.42.fr>
2019-11-06 18:27:15 +0100

    goto home

--- ft_printf.c
+++ ft_printf.c
@@ -6,70 +6,24 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:57:19 by juligonz          #+#    #+#             */
-/*   Updated: 2019/11/06 17:46:23 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/11/06 18:26:28 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "ft_printf.h"
 
-size_t	ft_strlen(const char *s)
+static int parse(const char c, va_list *args)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-static int	ft_putchar_fd(unsigned char c, int fd)
-{
-	write(fd, &c, 1);
-	return (1);
-}
-
-static	int	ft_putstr_range(char const *s_begin, char const *s_end)
-{
-	int	n;
-
-	n = s_end - s_begin;
-	write(1, s_begin, n);
-	return (n);
-}
-
-//cspdiuxX
-
-static int	parse(const char c, va_list *args)
-{
-	if (c == 'c')
-		return (ft_putchar_fd(va_arg(*args, int), 1));
-	if (c == 's')
-		return (ft_putstr(va_arg(*args, char*)));
-	if (c == 'p')
-	{
-		ft_putstr("0x");		
-		return (ft_putnbr_base_fd(va_arg(*args, long long), 16, 1, 0) + 2);
-	}
-	if (c == 'd' || c == 'i')
-		return (ft_putnbr_base_fd(va_arg(*args, long long), 10, 1, 0));
-//	if (c == 'i')
-//		return (0);
-	if (c == 'u')
-		return (ft_putnbr_base_fd(va_arg(*args, long long), 10, 1, 0));
-	if (c == 'x')
-		return (ft_putnbr_base_fd(va_arg(*args, long long), 16, 1, 0));
-	if (c == 'X')
-		return (ft_putnbr_base_fd(va_arg(*args, long long), 16, 1, 1));
-	if (c == '%')
-		return (ft_putchar_fd('%',1));
-	return (0);
-//nfge
+	
+	while ( )
 }
 
 static long long	flags(short flags, char *format)
 {
+	const char *convs = "cspdiux%Xnfge";
 	int i;
 
 	i = 0;
