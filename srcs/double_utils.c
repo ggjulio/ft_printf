d652293120bf634a0e5c41ce9122984b7ae3b5b8
ggjulio <juligonz@student.42.fr>
2019-11-26 19:19:54 +0100

    Apostrophe flag implemented in float

--- srcs/double_utils.c
+++ srcs/double_utils.c
@@ -6,19 +6,23 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 17:36:20 by juligonz          #+#    #+#             */
-/*   Updated: 2019/11/25 19:26:27 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/11/26 19:15:49 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void		put_exp(int64_t exp, t_manager *p)
+static void		put_exp(int64_t exp, t_manager *p, size_t idx)
 {
-	char c;
+	char	c;
 
 	c = exp % 10 + '0';
 	if (exp >= 10)
-		put_exp(exp / 10, p);
+	{
+		put_exp(exp / 10, p, idx + 1);
+		if (GET(F_APOSTROPHE) && idx % 3 == 0)
+			write_buffer(p, ",", 1);
+	}
 	write_buffer(p, &c, 1);
 }
 
@@ -45,7 +49,7 @@ long double		ft_round(long double n, size_t precision)
 
 void			put_double(long double n, t_manager *p)
 {
-	put_exp((int64_t)n, p);
+	put_exp((int64_t)n, p, 1);
 	if (p->precision > 0 || GET(F_HASH))
 		write_buffer(p, ".", 1);
 	if (p->precision > 0)
