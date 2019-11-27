ada2cac3a4e5e7e93d196eae03f33924bcbaed2c
ggjulio <juligonz@student.42.fr>
2019-11-27 16:32:27 +0100

    put_e precision fixed when negative number

--- main.c
+++ main.c
@@ -6,12 +6,11 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:47:31 by juligonz          #+#    #+#             */
-/*   Updated: 2019/11/26 19:17:39 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/11/27 16:17:17 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "ft_printf.h"
-#include "printf_proto.h"
+int ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
 
 #include <stdio.h>
 #include <locale.h>
@@ -90,22 +89,12 @@ int 	main(void)
 	ft_printf("%.19e\n", -9.000000005);
 
 	ft_printf("\n\n\n\n");
-	printf("%e\n", 9.00);
+	   printf("%e\n", 9.00);
 	ft_printf("%e\n", 9.00);
-	printf("%e\n", 10.00);
+	   printf("%e\n", 10.00);
 	ft_printf("%e\n", 10.00);
-	printf("%e\n", 101.00);
+       printf("%e\n", 101.00);
 	ft_printf("%e\n", 101.00);
-
-	printf("%g\n", 1.13);
-	ft_printf("%g\n", 1.13);
-	printf("%f\n", 1.13);
-	ft_printf("%f\n", 1.13);
-	printf("%e\n", 1.13);
-	ft_printf("%e\n", 1.13);
-
-	printf("%.15g\n", 1.1353255243);
-	printf("%g\n", 0.00003);
 
 	   printf("%e\n", 0.1);
 	ft_printf("%e\n", 0.1);
@@ -115,19 +104,86 @@ int 	main(void)
 	ft_printf("%e\n", 0.04);
 	   printf("%e\n", 0.0042);
 	ft_printf("%e\n", 0.0042);
-*/
-	printf("%#8.5o_\n", 34);
-	ft_printf("%#8.5o_\n", 34);
-	printf("%#-8.5o_\n", 34);
-	ft_printf("%#-8.5o_\n", 34);
 
-	printf("%'f_\n", 1234.735268);
-	ft_printf("%'f_\n", 1234.735268);
-	printf("%'f_\n", 12343243244.735268);
-	ft_printf("%'f_\n", 12343243244.735268);
-	ft_printf("%'f_\n", 124.735268);
-	printf("%'f_\n", 124.735268);
-	ft_printf("%'f_\n", 1234.735268);
-	printf("%'f_\n", 1234.735268);
-	ft_printf("%'f_\n", 1000000.00000);
+
+	printf("\n\n\n");
+	printf("\n\n\n");
+	   printf("%g\n", 1.13);
+	ft_printf("%g\n", 1.13);
+	   printf("%f\n", 1.13);
+	ft_printf("%f\n", 1.13);
+	   printf("%e\n", 1.13);
+	ft_printf("%e\n", 1.13);
+	   printf("%e\n", 1.13);
+	ft_printf("%e\n", 1.13);
+	printf("\n\n\n");
+
+
+*/
+//	printf("%#8.5o_\n", 34);
+//	ft_printf("%#8.5o_\n", 34);
+//	printf("%#-8.5o_\n", 34);
+//	ft_printf("%#-8.5o_\n", 34);
+
+
+	   printf("%-15e_\n", 1.13);
+	ft_printf("%-15e_\n", 1.13);
+	   printf("% -15e_\n", 1.13);
+	ft_printf("% -15e_\n", 1.13);
+	   printf("%15e_\n", 1.13);
+	ft_printf("%15e_\n", 1.13);
+	   printf("%015e_\n", 1.13);
+	ft_printf("%015e_\n", 1.13);
+	   printf("%e_\n", 1.135674726756437);
+	ft_printf("%e_\n", 1.135674726756437);
+       printf("%.7e_\n", 1.135674726756437);
+	ft_printf("%.7e_\n", 1.135674726756437);
+       printf("%.5e_\n", -61.135674726756437);
+    ft_printf("%.5e_\n", -61.135674726756437);
+       printf("%.0e_\n", 61.135674726756437);
+    ft_printf("%.0e_\n", 61.135674726756437);
+       printf("%.1e_\n", 13.135674726756437);
+    ft_printf("%.1e_\n", 13.135674726756437);
+       printf("%.15e_\n", 1.135674);
+    ft_printf("%.15e_\n", 1.135674);
+	   printf("%.0e_\n", (double)0);
+	ft_printf("%.0e_\n", (double)0);
+	   printf("%+.1e_\n", (double)1);
+	ft_printf("%+.1e_\n", (double)1);
+	   printf("%+.1e_\n", (double)-1);
+	ft_printf("%+.1e_\n", (double)-1);
+	   printf("%.1e_\n", (double)-1);
+	ft_printf("%.1e_\n", (double)-1);
+	   printf("%#.0e_\n", (double)0);
+	ft_printf("%#.0e_\n", (double)0);
+	   printf("%*e_\n", 5, 10.4324534);
+	ft_printf("%*e_\n", 5, 10.4324534);
+	   printf("%*e_\n", 15, 10.4324534);
+	ft_printf("%*e_\n", 15, 10.4324534);
+	   printf("%*.*e_\n", 15, 3, 10.4324534);
+	ft_printf("%*.*e_\n", 15, 3, 10.4324534);
+	   printf("% *.*e_\n", 15, 3, 10.4324534);
+	ft_printf("% *.*e_\n", 15, 3, 10.4324534);
+	   printf("% 0*.*e_\n", 15, 3, 10.4324534);
+	ft_printf("% 0*.*e_\n", 15, 3, 10.4324534);
+	   printf("%+0*.*e_\n", 15, 3, 10.4324534);
+	ft_printf("%+0*.*e_\n", 15, 3, 10.4324534);
+	   printf("%+*.*e_\n", 16, 3, 10.4324534);
+	ft_printf("%+*.*e_\n", 16, 3, 10.4324534);
+	   printf("%+*.*e_\n", 16, 3, -10.4324534);
+	ft_printf("%+*.*e_\n", 16, 3, -10.4324534);
+       printf("%.5e_\n", -0.00003567);
+	ft_printf("%.5e_\n", -0.00003567);
+
+	printf("\n\n\n\n");
+       printf("%.5e_\n", -61.13567);
+    ft_printf("%.5e_\n", -61.13567);
+       printf("%.6e_\n", -61.13567);
+    ft_printf("%.6e_\n", -61.13567);
+       printf("%.6e_\n", -100.13567);
+    ft_printf("%.6e_\n", -100.13567);
+
+       printf("%.6e_\n", -100.00006);
+    ft_printf("%.6e_\n", -100.00006);
+
 }
--- srcs/put_e.c
+++ srcs/put_e.c
@@ -6,16 +6,46 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 15:30:53 by juligonz          #+#    #+#             */
-/*   Updated: 2019/11/27 14:57:37 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/11/27 16:30:47 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	get_e(double n)
+static void	put_width(t_manager *p, int nb_char)
+{
+	int i;
+
+	if (GET(F_ZERO))
+		return ;
+	i = p->width;
+	while (i > nb_char)
+	{
+		write_buffer(p, " ", 1);
+		i--;
+	}
+}
+
+static void	put_zero(t_manager *p, int nb_char)
+{
+	int i;
+
+	if (!GET(F_ZERO))
+		return ;
+	i = p->width;
+	while (i > nb_char)
+	{
+		write_buffer(p, "0", 1);
+		i--;
+	}
+}
+
+static int	get_e(long double n)
 {
 	int e;
 
+	if (!n)
+		return (0);
 	e = 0;
 	while (n >= 10)
 	{
@@ -28,6 +58,16 @@ static int	get_e(double n)
 		n *= 10;
 	}
 	return (e);
+}
+
+static void	update_n(long double *n, int e)
+{
+	if (e > 0)
+		while (e-- != 0)
+			*n /= 10;
+	else
+		while (e++ != 0)
+			*n *= 10;
 }
 
 static void	show_exp(int e, t_manager *p)
@@ -45,23 +85,25 @@ static void	show_exp(int e, t_manager *p)
 void		put_e(long double n, t_manager *p)
 {
 	char	is_neg;
-	int		i;
 	int		e;
+	int		nb_char;
 
 	p->precision = (GET(F_DOT) ? p->precision : 6);
 	is_neg = (n < 0 ? 1 : 0);
 	n = (n < 0 ? -n : n);
 	e = get_e(n);
-	n = ft_round(n, p->precision + (e < 0 ? -e : e));
+	n = ft_round(n, p->precision + (e < 0 || is_neg ? -e : e));
+	update_n(&n, e);
+	nb_char = p->precision + 6 + (GET(F_PLUS) ? 1 : 0);
+	if ((GET(F_SPACE) && !(is_neg || GET(F_PLUS))) && (p->width-- || 1))
+		write_buffer(p, " ", 1);
+	if (!GET(F_DASH))
+		put_width(p, nb_char);
 	if (is_neg || GET(F_PLUS))
 		write_buffer(p, (is_neg ? "-" : "+"), 1);
-	i = e;
-	if (i > 0)
-		while (i-- != 0)
-			n /= 10;
-	else
-		while (i++ != 0)
-			n *= 10;
+	put_zero(p, nb_char);
 	put_double(n, p);
 	show_exp(e, p);
+	if (GET(F_DASH))
+		put_width(p, nb_char);
 }
