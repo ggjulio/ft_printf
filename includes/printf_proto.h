f0d6e61b5a874e7da63b7faab2a9a5c1097be1bc
ggjulio <juligonz@student.42.fr>
2019-11-27 14:41:00 +0100

    norm fixed

--- includes/printf_proto.h
+++ includes/printf_proto.h
@@ -6,14 +6,13 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 20:46:39 by juligonz          #+#    #+#             */
-/*   Updated: 2019/11/25 20:47:08 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/11/27 14:40:37 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef PRINTF_PROTO_H
 # define PRINTF_PROTO_H
 
-int				ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
+int	ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
 
 #endif
