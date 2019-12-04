26f1c8926dd29c11a041753be9b656f94dd29058
ggjulio <juligonz@student.42.fr>
2019-12-04 15:55:16 +0100

    include srdarg.h added to printf_proto.h

--- includes/printf_proto.h
+++ includes/printf_proto.h
@@ -6,12 +6,14 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 20:46:39 by juligonz          #+#    #+#             */
-/*   Updated: 2019/12/02 17:01:35 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/12/03 20:35:09 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTF_PROTO_H
 # define PRINTF_PROTO_H
+
+# include <stdarg.h>
 
 int	ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
 int	ft_dprintf(
