7e5a0afd53d3f5a341ce5409fe8839f7feee2f0e
ggjulio <juligonz@student.42.fr>
2019-12-10 11:50:19 +0100

    goto school a pied (10km 2h de marche (greves dec 2019) :D)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:47:31 by juligonz          #+#    #+#             */
-/*   Updated: 2019/12/04 21:03:07 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/12/08 19:34:52 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
 #include <float.h>
 #include <limits.h>
 #include <stdint.h>
+#include <locale.h>
 
 #include <wchar.h>
 
@@ -23,14 +24,46 @@ int ft_printf(const char *format, ...) __attribute__((format(printf,1,2)));
 int 	main(void)
 {
 	uint64_t n = ULONG_MAX;
-
 	(void)n;
+//	locale_changed =  setlocale(LC_NUMERIC, "en_US");
+	
 
-	wchar_t ws[50] = L"Salut Gérard !";
-	wchar_t wc = ws[7];
+	char *locale_changed;
+	locale_changed = setlocale(LC_ALL, "fr_FR.UTF-8");
+//	locale_changed = setlocale(LC_ALL, "");
+//	locale_changed = setlocale(LC_ALL, "chs");
+//	locale_changed = setlocale(LC_ALL, "chinese-simplified");
 
-	   printf( "%ls_\n", ws);
-	ft_printf( "%ls_\n", ws);
+	if (locale_changed == NULL)
+		printf("\n|||||||||| ERROR : Unable to set locale ||||||||\n");
+	
+	wchar_t *ws = L"Salut Gérard !";
+	char *s = "Salut Gérard !";
+
+	printf("____________________________________________\n");
+	printf("|||||||||||||||||||TEST|||||||||||||||||||||\n");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+	printf("|%d|r\n",    printf("Grâve."));
+	printf("|%d|m\n", ft_printf("Grâve."));
+	   
+    ft_printf("++++++++++++++++++++++++===\n");
+	
+	printf("~~~~~~~~~~~~~~\n");
+	printf("|%d|r\n",    printf("%s", s));
+	printf("|%d|m\n", ft_printf("%s", s));
+	
+	printf("~~~~~~~~~~~~~~\n");
+	printf("|%d|r\n",    printf("%ls", ws));
+	printf("|%d|m\n", ft_printf("%ls", ws));
+
+	printf("~~~~~~~~~~~~~~\n");
+	printf("|%d|r\n",    printf("%S", ws));
+	printf("|%d|m\n", ft_printf("%S", ws));
+
+	
+//	   printf( "%lc_\n", wc);
+//	ft_printf( "%ls_\n", ws);
 //	   printf( "%lc_\n", wc);
 //	ft_printf( "%lc_\n", wc);
 	
--- srcs/convs/conv_s.c
+++ srcs/convs/conv_s.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 19:15:12 by juligonz          #+#    #+#             */
-/*   Updated: 2019/12/04 21:07:09 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/12/05 16:09:53 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void		conv_s(va_list args, t_manager *p)
 {
-	wchar_t	*s;
+	char	*s;
 	int		len;
 
-//	s = va_arg(args, char*);
-	s = cast_s(p, args);
+	s = va_arg(args, char*);
+//	s = cast_s(p, args);
 	if (!s)
-		s = L"(null)";
+		s = "(null)";
 	len = ft_strlen(s);
 	if (F_DOT & p->flags && p->precision < len && p->precision >= 0)
 		len = p->precision;
